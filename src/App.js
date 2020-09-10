31f31f0e2304ad957f717382dace85739f6fb103
Andrew <andrewdddo@gmail.com>
2020-09-10 22:03:08 +1000

    Set up project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +20,40 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+export default App;*/
+
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+//Pages
+import HomePage from "./pages/home.js";
+
+//Components
+//import Navigation from "./components/navigation";
+
+
+function AppRouter(props) {
+  return (
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+      </Switch>
+  );
+}
+
+function App() {
+  return (
+      <div className="App">
+          <BrowserRouter>
+            <div>
+              <AppRouter />
+            </div>
+          </BrowserRouter>
+      </div>
   );
 }
 
